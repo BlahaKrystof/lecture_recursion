e11e3a9f66af7d8ae7f2a10801fb1950211091c6
Krystof Blaha <blahakrystof3@gmail.com>
2025-04-28 10:33:20 +0200

    c

--- flood_fill.py
+++ flood_fill.py
@@ -1,12 +1,23 @@
 import matplotlib.pyplot as plt
 
+def flood_fill(img, x, y):
+    if x in range(len(img[0])) and y in range(len(img)):
+        if  img[x][y] == 1.:
+            img[x][y] = 2.
+            flood_fill(img, x +1, y)
+            flood_fill(img, x, y +1)
+            flood_fill(img, x + 1, y + 1)
+        elif img[x][y] == 0.:
+            img[x][y] = 0.
+
+
 
 def main():
     img = plt.imread("files/img0.png")[:, :, 0]
     # img = plt.imread("files/img1.png")[:, :, 0]
     # img = plt.imread("files/img2.png")[:, :, 0]
 
-    # img = floodfill(img, 0, 0)
+    img = flood_fill(img, 0, 0)
 
     plt.imshow(img, cmap="gray")
     plt.show(block=False)
