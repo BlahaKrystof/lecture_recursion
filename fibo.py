ea6885c87ee18f1d72f4bdc81f2a467ebb2f8499
Krystof Blaha <blahakrystof3@gmail.com>
2025-04-28 10:20:08 +0200

    a

--- fibo.py
+++ fibo.py
@@ -1,7 +1,23 @@
+def recursive_nth_fibo(n):
+
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return recursive_nth_fibo(n - 1) + recursive_nth_fibo(n - 2)
+
+
+
 
 def main():
-    pass
+    n = int(input("Zadej pořadí prvku fibonacciho posloupnosti: "))
+    print(recursive_nth_fibo(n))
+    fib_list = []
+    for m in range(n + 1):
+        fib_list.append(recursive_nth_fibo(m))
 
+    print(fib_list)
 
 if __name__ == "__main__":
     main()
